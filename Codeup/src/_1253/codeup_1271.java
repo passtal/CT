f914b696826851b8bd1758cf8525665095ec45de
tj-bu-706-09 <tj-bu-706-09@DESKTOP-ODRLT0H>
2025-10-23 18:29:40 +0900

    codeup

---
+++ Codeup/src/_1253/codeup_1271.java
@@ -0,0 +1,36 @@
+package _1253;
+
+import java.util.Scanner;
+
+public class codeup_1271 {
+	
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner (System.in);
+		int n = sc.nextInt();
+		
+		int max = 0;
+		
+		for (int i = 0; i < n ; i++) {
+			int input = sc.nextInt();
+			
+			if (max < input) {
+				max = input;
+			}
+		}
+				
+		System.out.print(max);		
+		sc.close();
+	}	
+
+}	
+	
+
+
+
+
+
+
+
+
+
